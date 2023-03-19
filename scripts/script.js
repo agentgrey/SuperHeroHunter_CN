28f8e1d1eb61d5c58a0c33a654e7030e3be22ba4
agentgrey <himadrinayak2702@gmail.com>
2023-03-20 00:48:03 +0530

    fetch API

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,4 @@
-console.log("script.js working");
-
-// PUBLIC KEY
-let privateKey= "c6aaa062a457b78d8f89781c181dc663c580d020";
-
-// PRIVATE KEY
-let publicKey = "39908c7590bdb2b8949f4df91087a9b8";
-
-// let ts = new Date().getTime();
-// var MD5 = require("crypto-js/md5"); 
-// console.log(MD5("text to hash").toString());
-
-// HASH
-let hash = "27703945b9bceacb09546d2e103ad360";
-
-
-
+// console.log("script.js working");
 
 // variables created from index.html
 var searchBar = document.getElementById("searchbar");
@@ -25,14 +9,51 @@ searchBar.addEventListener("input", () => searchHeros(searchBar.value));
 
 // function for API call
 async function searchHeros(heroname) {
-    // console.log("i am in searchHeros"); 
+    console.log("i am in searchHeros " +heroname); 
 
-    // API call to get data
-    // showResults(heroname);
-    await fetch(`https://gateway.marvel.com/v1/public/characters?nameStartsWith=${heroname}&apikey=${publicKey}&hash=${hash}`);
+// if input is non empty
+    if(heroname.length > 0) {
+        let url="https://gateway.marvel.com/v1/public/characters?nameStartsWith="+heroname+"&limit=8&ts=1&apikey=479af95fea303989d02e4a22757147e7&hash=b837dd71ea30a412d884f205f4da646f";
+
+        await fetch(url).then(function (response) {
+                        return response.json()
+                    })
+                    .then(function (data) {
+                        showResults(data);
+                    });;
+    } 
+// if input is empty
+    else {
+        console.log("empty");
+        searchResults.innerHTML = "";
+    }
 }
 
 // function for displaying search results
-function showResults(heroname) {
-    console.log(heroname);
+function showResults(d) {
+    console.log("i am in showResults");
+    res = d.data.results;
+    // console.log(res);
+
+    let html=``;
+    if(res) {
+        html = res.map(element => {
+            return `
+                <div class="card">
+                <img id="poster" class="poster" src="${element.thumbnail.path+'/portrait_medium.' + element.thumbnail.extension}">
+                <div class="details">
+                <a href="single.html" style="text-decoration: none; color: inherit;">
+                <p id="card-name" class="card-name"> ${element.name} </p></a>
+                <div class="icon">
+                    <i class="heart-icon fa-regular fa-heart"></i>
+                    <i class="heart-icon fa-solid fa-heart"></i>
+                </div>
+                </div>
+                </div>
+            `
+        }).join('');
+        searchResults.innerHTML = html;
+    } else {
+        searchResults.innerHTML = "";
+    }
 }
