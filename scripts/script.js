9eecf9b1df9ff8f15966a704930e3b231158af27
agentgrey <himadrinayak2702@gmail.com>
2023-03-20 12:53:10 +0530

    single page

--- scripts/script.js
+++ scripts/script.js
@@ -33,7 +33,7 @@ async function searchHeros(heroname) {
 function showResults(d) {
     console.log("i am in showResults");
     res = d.data.results;
-    // console.log(res);
+    console.log(res);
 
     let html=``;
     if(res) {
@@ -43,7 +43,20 @@ function showResults(d) {
                 <img id="poster" class="poster" src="${element.thumbnail.path+'/portrait_medium.' + element.thumbnail.extension}">
                 <div class="details">
                 <a href="single.html" style="text-decoration: none; color: inherit;">
-                <p id="card-name" class="card-name"> ${element.name} </p></a>
+                    <p id="cardName" class="cardName"> ${element.name} </p>
+                    <div style="display:none;">
+                        <span>${element.name}</span>
+                        <span>${element.description}</span>
+                        <span>${element.comics.available}</span>
+                        <span>${element.series.available}</span>
+                        <span>${element.stories.available}</span>
+                        <span>${element.thumbnail.path+'/portrait_uncanny.' + element.thumbnail.extension}</span>
+                        <span>${element.id}</span>
+                        <span>${element.thumbnail.path+'/landscape_incredible.' + element.thumbnail.extension}</span>
+                        <span>${element.thumbnail.path+'/standard_fantastic.' + element.thumbnail.extension}</span>
+                </div>
+                </a>
+                
                 <div class="icon">
                     <i class="heart-icon fa-regular fa-heart"></i>
                     <i class="heart-icon fa-solid fa-heart"></i>
@@ -56,4 +69,32 @@ function showResults(d) {
     } else {
         searchResults.innerHTML = "";
     }
+
+// if searchBar is empty then show nothing in searchResults
+    if(searchBar.value.length==0) {
+        console.log("empty in searchResults")
+        searchResults.innerHTML = "";
+    }
+    events();
+}
+
+
+
+// Function for attacthing eventListener to buttons
+function events() {
+     let characterInfo = document.querySelectorAll(".cardName");
+     characterInfo.forEach((character) => addEventListener('click', openSinglePage(character)))
+}
+
+// Function which stores the info object of character for which user want to see the info 
+function openSinglePage(c) {
+// This function basically stores the data of character in localStorage.
+// When user clicks on the name, the info page is opened that fetches the heroInfo and displays the data  
+// console.log("in openSinglePage");
+// console.log(c.parentElement.children[1]);
+     let heroInfo = {
+         name: c.innerHTML, desc: c.parentElement.children[1].children[1].innerHTML, comic: c.parentElement.children[1].children[2].innerHTML, series: c.parentElement.children[1].children[3].innerHTML, stories: c.parentElement.children[1].children[4].innerHTML, img: c.parentElement.children[1].children[5].innerHTML
+     }
+    // console.log(heroInfo);
+     localStorage.setItem("heroInfo", JSON.stringify(heroInfo));
 }
---
+++ scripts/single.js
@@ -0,0 +1,12 @@
+console.log("i am in single.js")
+
+let heroInfo = JSON.parse(localStorage.getItem("heroInfo"));
+
+// variables from single.html
+var title = document.getElementById("title");
+var photo = document.getElementById("photo");
+
+
+title.innerHTML =  heroInfo.name;
+photo.setAttribute("src", heroInfo.img);
+// console.log(heroInfo);
